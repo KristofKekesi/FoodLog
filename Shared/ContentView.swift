8826cfec0276ce5e9ff13eafbdea057a0bc05d96
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2022-02-24 23:40:53 +0100

    Update ContentView.swift

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -32,6 +32,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView().previewDisplayName("Main")
+		ContentView().previewDisplayName("MainView")
 	}
 }
